1ab45d0c3110d44a6072133926c0efc07415398f
Justin Morgan <justin.morgan@hey.com>
2021-10-11 13:17:39 -0700

    :pencil2: (typeorm) Fixed type of param user -> username

--- ormconfig.js
+++ ormconfig.js
@@ -12,7 +12,7 @@ if (process.env.NODE_ENV === 'production') {
   module.exports = {
     ...baseConfig,
     host: process.env.DB_HOST,
-    user: process.env.DB_USER,
+    username: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     port: 5432,
     database: process.env.DB_DATABASE,
