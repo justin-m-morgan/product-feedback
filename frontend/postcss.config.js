c26ec21523aa7ab6940b309e0608425c0c25af26
Justin Morgan <justin.morgan@hey.com>
2021-10-13 06:03:49 -0700

    :heavy_plus_sign:  Add PostCSS config and plugins
    Added postcss-nested, postcss-media-variables, autoprefixer
    
    Closes #14

---
+++ frontend/postcss.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  plugins: [
+    require('postcss-media-variables'),
+    require('autoprefixer'),
+    require('postcss-nested'),
+    require('postcss-media-variables'),
+  ],
+};
