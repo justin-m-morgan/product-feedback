6260792e3b275c0df3dd8682c7f10564590d2ffb
Justin Morgan <justin.morgan@hey.com>
2021-10-16 11:27:13 -0700

    :construction: (TODO) Add ToggleHide component for normalizing el toggle

---
+++ frontend/src/interactions/ToggleHide.tsx
@@ -0,0 +1,12 @@
+import React, { useState } from 'react';
+import classnames from 'classnames';
+
+// function ToggleHide() {
+//     return (
+//         <div className={cx('container')}>
+//             <TriggerElement />
+//             <RevealElement />
+
+//         </div>
+//     )
+// }
