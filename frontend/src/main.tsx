5332fbb8df1cd0314f2f4338b8f05a7a1ce0cc44
Justin Morgan <justin.morgan@hey.com>
2021-10-16 11:27:52 -0700

    :fire:  Cleaning up scaffolded entry point file

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,8 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import './reset.css';
-import './index.css';
-import './typograph.css';
+import './css/index.css';
+
 import App from './App'
 
 ReactDOM.render(
