95817bfcb3debd86f1412330ebb9c75f5c732475
Justin Morgan <justin.morgan@hey.com>
2021-10-17 07:24:41 -0700

    :wrench: (storybook) Change background colour to off-white

--- frontend/.storybook/preview.js
+++ frontend/.storybook/preview.js
@@ -3,6 +3,15 @@ import '../src/css/index.css';
 
 export const parameters = {
   actions: { argTypesRegex: '^on[A-Z].*' },
+  backgrounds: {
+    default: 'light-gray',
+    values: [
+      {
+        name: 'light-gray',
+        value: 'hsl(231, 100%, 97%)',
+      },
+    ],
+  },
   controls: {
     matchers: {
       color: /(background|color)$/i,
