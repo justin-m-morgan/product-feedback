7cd35bf1cf1f2dd4923a1041f86b70c2f5bc7265
Justin Morgan <justin.morgan@hey.com>
2021-10-16 20:54:15 -0700

    :lipstick:  Implement category filter card

---
+++ frontend/src/elements/CategoryFilter.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import { ComponentStory, ComponentMeta } from '@storybook/react';
+
+import CategoryFilter, { Category } from './CategoryFilter';
+
+export default {
+  title: 'Elements/CategoryFilter',
+  component: CategoryFilter,
+} as ComponentMeta<typeof CategoryFilter>;
+
+const Template: ComponentStory<typeof CategoryFilter> = (args) => (
+  <CategoryFilter {...args} />
+);
+
+export const Primary = Template.bind({});
+Primary.args = {
+  active: Category.Enhancement,
+};
